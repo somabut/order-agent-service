c131caacc3ff89c9d604a7000a32d7b400c1ae25
moodTRBL <sonickim6679@gmail.com>
2025-07-25 16:11:40 +0900

    FEAT: 응답의 httpStatus 역직렬화 기능 추가

--- src/main/kotlin/com/orderagentservice/global/model/response/ApiResponse.kt
+++ src/main/kotlin/com/orderagentservice/global/model/response/ApiResponse.kt
@@ -1,14 +1,17 @@
 package com.orderagentservice.global.model.response
 
 import com.fasterxml.jackson.annotation.JsonIgnore
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize
 import com.orderagentservice.global.model.dto.ExceptionDto
 import com.orderagentservice.global.model.RootException
+import com.orderagentservice.global.util.HttpStatusFlexibleDeserializer
 import org.springframework.http.HttpStatus
 
 data class ApiResponse<T> (
     val success: Boolean,
     val data: T?,
     val error: ExceptionDto?,
+    @JsonDeserialize(using = HttpStatusFlexibleDeserializer::class)
     val httpStatus: HttpStatus?
 ) {
     companion object {
