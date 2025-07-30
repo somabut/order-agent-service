7604ad950291e7a8291adc1e64c8a776ee1bb749
moodTRBL <sonickim6679@gmail.com>
2025-07-30 20:55:06 +0900

    FIX: jep 다중 설정 문제 해결

--- src/main/kotlin/com/orderagentservice/order/model/request/ActionReplyRequest.kt
+++ src/main/kotlin/com/orderagentservice/order/model/request/ActionReplyRequest.kt
@@ -8,29 +8,29 @@ data class ActionReplyRequest(
 )
 
 data class CartData(
-    val kioskType: String,
-    val level: Int,
-    val currentPage: String,
-    val cartItems: List<Item>,
-    val totalPrice: Int,
+    val kioskType: String?,
+    val level: Int?,
+    val currentPage: String?,
+    val cartItems: List<Item>?,
+    val totalPrice: Int?,
     val paymentMethod: String?
 )
 
 data class Changes(
-    val type: String,
-    val description: String,
-    val details: List<Details>
+    val type: String?,
+    val description: String?,
+    val details: List<Details>?
 )
 
 data class Details(
-    val action: String,
+    val action: String?,
     val item: Item?
 )
 
 data class Item(
-    val name: String,
-    val price: Int,
-    val quantity: Int,
-    val total: Int,
-    val type: String
+    val name: String?,
+    val price: Int?,
+    val quantity: Int?,
+    val total: Int?,
+    val type: String?
 )
