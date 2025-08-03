6717a026dc2d486cb590869b38e14a7d7f5f4019
moodTRBL <sonickim6679@gmail.com>
2025-08-03 23:30:18 +0900

    FIX: 자동 결제 중에 노드 누락되는 버그 수정

--- src/main/kotlin/com/orderagentservice/order/model/dto/ActionPathDto.kt
+++ src/main/kotlin/com/orderagentservice/order/model/dto/ActionPathDto.kt
@@ -5,4 +5,15 @@ data class ActionPathDto(
     val title: String,
     val x: Int,
     val y: Int
-)
+) {
+    companion object {
+        fun toPathDtoList(nodes: List<Map<String, Any>>): List<ActionPathDto> {
+            return nodes
+                .map { node -> ActionPathDto(
+                    node["id"].toString(), node["title"].toString(),
+                    x = (node["x"] as Number).toInt(), y = (node["y"] as Number).toInt()
+                ) }
+        }
+    }
+
+}
