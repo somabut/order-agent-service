762a4bbf84ed64138edbf553cb7a8cadda3043a6
moodtrbl <sonickim6679@gmail.com>
2025-09-09 16:58:26 +0900

    FIX: back to관계 제대로 설정되지 않았던 오류 수정

--- src/main/kotlin/com/orderagentservice/order/service/utg/UiDetectorManager.kt
+++ src/main/kotlin/com/orderagentservice/order/service/utg/UiDetectorManager.kt
@@ -42,8 +42,8 @@ class UiDetectorManager @Autowired constructor(
         val imageType = captureDto.type
         val uiResponse = queryUiExtractor(imageBytes, imageType)
 
-        val uiElements = uiResponse.uiComponents
-//        val uiElements = uiResponse.ocrComponents
+//        val uiElements = uiResponse.uiComponents
+        val uiElements = uiResponse.ocrComponents
         val ocrElements = uiResponse.ocrComponents
         val yoloElements = uiResponse.yoloComponents
 
