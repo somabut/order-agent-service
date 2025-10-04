a022167b82227ff5574d9a1fa01c5bad933569cc
moodTRBL <sonickim6679@gmail.com>
2025-10-05 02:06:59 +0900

    FEAT: update UTG 전략 패턴에 따라 API추가

--- src/main/kotlin/com/orderagentservice/order/controller/UtgControllerV2.kt
+++ src/main/kotlin/com/orderagentservice/order/controller/UtgControllerV2.kt
@@ -5,6 +5,8 @@ import com.orderagentservice.global.model.response.ApiResponse
 import com.orderagentservice.logger
 import com.orderagentservice.order.exception.KioskAdminSignInException
 import com.orderagentservice.order.model.UtgContext
+import com.orderagentservice.order.model.request.CategoryUtgUpdateRequest
+import com.orderagentservice.order.model.request.MenuUtgUpdateRequest
 import com.orderagentservice.order.model.request.UtgStrategyRequest
 import com.orderagentservice.order.model.type.OverlayType
 import com.orderagentservice.order.service.NotificationService
@@ -35,6 +37,53 @@ class UtgControllerV2 @Autowired constructor(
         notificationService.sendOverlayCommand(kioskId, OverlayType.UTG_END.title)
 
         log.info("전체 토큰 사용량: ${usageTracker.totalUsage}")
+        return ApiResponse.success(history)
+    }
+
+    @PostMapping("/update/category/{kioskId}")
+    fun updateCategoryUtg(
+        @PathVariable kioskId: String,
+        @RequestBody categoryUtgUpdateRequest: CategoryUtgUpdateRequest,
+        @RequestHeader("Authorization", required = false) accessToken: String?
+    ): ApiResponse<*> {
+        if (accessToken == null) throw KioskAdminSignInException()
+
+        notificationService.sendOverlayCommand(kioskId, OverlayType.UTG_START.title)
+        val history = utgService.updateCategory(
+            kioskId = kioskId, accessToken = accessToken,
+            isInitPayment = categoryUtgUpdateRequest.initPayment
+        )
+        notificationService.sendOverlayCommand(kioskId, OverlayType.UTG_END.title)
+
+        return ApiResponse.success(history)
+    }
+
+    @PostMapping("/update/menu/{kioskId}")
+    fun updateMenuUtg(
+        @PathVariable kioskId: String,
+        @RequestBody menuUtgUpdateRequest: MenuUtgUpdateRequest,
+        @RequestHeader("Authorization", required = false) accessToken: String?
+    ): ApiResponse<*> {
+        if (accessToken == null) throw KioskAdminSignInException()
+
+        notificationService.sendOverlayCommand(kioskId, OverlayType.UTG_START.title)
+        val history = utgService.updateMenu(
+            kioskId = kioskId, accessToken = accessToken,
+            isInitPayment = menuUtgUpdateRequest.initPayment
+        )
+        notificationService.sendOverlayCommand(kioskId, OverlayType.UTG_END.title)
+
+        return ApiResponse.success(history)
+    }
+
+    @PostMapping("/update/payment/{kioskId}")
+    fun updatePaymentUtg(
+        @PathVariable kioskId: String,
+    ): ApiResponse<*> {
+        notificationService.sendOverlayCommand(kioskId, OverlayType.UTG_START.title)
+        val history = utgService.updatePayment(kioskId)
+        notificationService.sendOverlayCommand(kioskId, OverlayType.UTG_END.title)
+
         return ApiResponse.success(history)
     }
 }
--- src/main/kotlin/com/orderagentservice/order/service/utg/UtgService.kt
+++ src/main/kotlin/com/orderagentservice/order/service/utg/UtgService.kt
@@ -3,16 +3,22 @@ package com.orderagentservice.order.service.utg
 import com.orderagentservice.agent.model.UsageTracker
 import com.orderagentservice.agent.model.dto.AgentActionDto
 import com.orderagentservice.global.service.LogService
+import com.orderagentservice.logger
+import com.orderagentservice.order.model.UtgActionProfile
 import com.orderagentservice.order.model.UtgContext
+import com.orderagentservice.order.model.dto.MenuInfoDto
 import com.orderagentservice.order.model.log.UtgEndLog
 import com.orderagentservice.order.model.log.UtgStartLog
 import com.orderagentservice.order.model.request.UtgStrategyRequest
 import com.orderagentservice.order.model.type.LogicType
+import com.orderagentservice.order.model.type.NodeType
 import com.orderagentservice.order.model.type.UtgForLogType
 import com.orderagentservice.order.service.MenuService
+import com.orderagentservice.order.service.graph.ui.UiGraphService
 import com.orderagentservice.order.service.utg.menu.MenuUtgService
 import com.orderagentservice.order.service.utg.payment.PaymentUtgService
 import com.orderagentservice.order.service.utg.strategy.UtgInitializeOrchestrator
+import com.orderagentservice.order.service.utg.strategy.UtgUpdateOrchestrator
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 
@@ -21,43 +27,47 @@ class UtgService @Autowired constructor(
     private val menuService: MenuService,
     private val menuUtgService: MenuUtgService,
     private val paymentUtgService: PaymentUtgService,
+    private val uiGraphService: UiGraphService,
     private val logService: LogService,
     private val usageTracker: UsageTracker,
 
-    private val utgInitializeOrchestrator: UtgInitializeOrchestrator
+    private val utgInitializeOrchestrator: UtgInitializeOrchestrator,
+    private val utgUpdateOrchestrator: UtgUpdateOrchestrator
 ) {
-    fun initializeGraph(kioskId: String, accessToken: String): List<AgentActionDto> {
-        logService.printLog(
-            UtgStartLog(
-                kioskId = kioskId,
-                utgForLogType = UtgForLogType.TOTAL
-            )
-        )
-        val startTime = System.nanoTime()
+    private val log = logger()
 
-        //관리자 페이지로부터 메뉴를 얻어온다
-        val menuList = menuService.getMenus(kioskId, accessToken)
-
-        val context = UtgContext.toBasicContext(kioskId)
-
-        menuUtgService.initializeGraph(
-            context = context,
-            menuList = menuList
-        )
-        paymentUtgService.initializeGraph(context = context)
-
-        val endTime = System.nanoTime()
-        logService.printLog(
-            UtgEndLog(
-                kioskId = context.kioskId,
-                utgForLogType = UtgForLogType.TOTAL,
-                processingTime = (endTime - startTime) / 1000000,
-                totalTokenUsage = usageTracker.totalUsage
-            )
-        )
-
-        return context.history
-    }
+//    fun initializeGraph(kioskId: String, accessToken: String): List<AgentActionDto> {
+//        logService.printLog(
+//            UtgStartLog(
+//                kioskId = kioskId,
+//                utgForLogType = UtgForLogType.TOTAL
+//            )
+//        )
+//        val startTime = System.nanoTime()
+//
+//        //관리자 페이지로부터 메뉴를 얻어온다
+//        val menuList = menuService.getMenus(kioskId, accessToken)
+//
+//        val context = UtgContext.toBasicContext(kioskId)
+//
+//        menuUtgService.initializeGraph(
+//            context = context,
+//            menuList = menuList
+//        )
+//        paymentUtgService.initializeGraph(context = context)
+//
+//        val endTime = System.nanoTime()
+//        logService.printLog(
+//            UtgEndLog(
+//                kioskId = context.kioskId,
+//                utgForLogType = UtgForLogType.TOTAL,
+//                processingTime = (endTime - startTime) / 1000000,
+//                totalTokenUsage = usageTracker.totalUsage
+//            )
+//        )
+//
+//        return context.history
+//    }
 
     fun init(kioskId: String, accessToken: String, utgStrategyRequest: UtgStrategyRequest): List<AgentActionDto> {
         logService.printLog(
@@ -85,6 +95,71 @@ class UtgService @Autowired constructor(
         )
 
         return context.history
+    }
+
+    fun updateCategory(
+        kioskId: String, accessToken: String,
+        isInitPayment: Boolean
+    ): List<String> {
+        val context = UtgContext.toBasicContext(kioskId, LogicType.UPDATE)
+        val menuList = menuService.getMenus(kioskId, accessToken)
+
+        //수정된 카테고리까지 가서 메뉴 노드 그리기
+        log.info("카테고리를 수정합니다")
+        val uiDtoList = uiGraphService.findModified(context.kioskId)
+
+        val modifiedCategoryList = uiDtoList
+            .filter { it.type == NodeType.CATEGORY }
+            .map { it.title }
+        val pendingList = menuList.filter { it.category in modifiedCategoryList }
+
+        utgUpdateOrchestrator.editCategories(
+            context = context,
+            modifiedCategoryList = modifiedCategoryList, pendingList = pendingList, menuList = menuList,
+            isInitPayment = isInitPayment
+        )
+
+        return context.pushedImages
+    }
+
+    fun updateMenu(
+        kioskId: String, accessToken: String,
+        isInitPayment: Boolean
+    ): List<String> {
+        val context = UtgContext.toBasicContext(kioskId, LogicType.UPDATE)
+        val menuList = menuService.getMenus(kioskId, accessToken)
+
+        //수정된 메뉴까지 가서 옵션 노드 그리기
+        log.info("메뉴를 수정합니다")
+        val uiDtoList = uiGraphService.findModified(context.kioskId)
+
+        val modifiedMenuList = uiDtoList
+            .filter { it.type == NodeType.MENU }
+            .map { it.title }
+        val pendingList = menuList.filter { it.title in modifiedMenuList }
+
+        utgUpdateOrchestrator.editMenus(
+            context = context,
+            pendingList = pendingList, menuList = menuList, modifiedMenuList = modifiedMenuList,
+            isInitPayment = isInitPayment
+        )
+
+        return context.pushedImages
+    }
+
+    fun updatePayment(kioskId: String): List<String> {
+        val context = UtgContext.toBasicContext(kioskId, LogicType.UPDATE)
+
+        val uiDtoList = uiGraphService.findModified(context.kioskId)
+        val modifiedPayment = uiDtoList
+            .filter { it.type == NodeType.PAYMENT }
+            .map { it.title }
+            .first()
+
+        utgUpdateOrchestrator.editPayment(context, modifiedPayment)
+        uiGraphService.changeModified(context.kioskId, modifiedPayment)
+
+        return context.pushedImages
     }
 
     fun updateCategoryGraph(
