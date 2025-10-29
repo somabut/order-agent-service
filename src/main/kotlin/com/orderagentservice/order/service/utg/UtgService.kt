331a8d31fc371ea6d5af7d25a075bd76ea989959
moodtrbl <sonickim6679@gmail.com>
2025-10-29 17:10:39 +0900

    TEST: compare manager 테스트 추가

--- src/main/kotlin/com/orderagentservice/order/service/utg/UtgService.kt
+++ src/main/kotlin/com/orderagentservice/order/service/utg/UtgService.kt
@@ -33,40 +33,6 @@ class UtgService @Autowired constructor(
     private val utgUpdateOrchestrator: UtgUpdateOrchestrator
 ) {
     private val log = logger()
-
-//    fun initializeGraph(kioskId: String, accessToken: String): List<AgentActionDto> {
-//        logService.printLog(
-//            UtgStartLog(
-//                kioskId = kioskId,
-//                utgForLogType = UtgForLogType.TOTAL
-//            )
-//        )
-//        val startTime = System.nanoTime()
-//
-//        //관리자 페이지로부터 메뉴를 얻어온다
-//        val menuList = menuService.getMenus(kioskId, accessToken)
-//
-//        val context = UtgContext.toBasicContext(kioskId)
-//
-//        menuUtgService.initializeGraph(
-//            context = context,
-//            menuList = menuList
-//        )
-//        paymentUtgService.initializeGraph(context = context)
-//
-//        val endTime = System.nanoTime()
-//        logService.printLog(
-//            UtgEndLog(
-//                kioskId = context.kioskId,
-//                utgForLogType = UtgForLogType.TOTAL,
-//                processingTime = (endTime - startTime) / 1000000,
-//                totalTokenUsage = usageTracker.totalUsage
-//            )
-//        )
-//
-//        return context.history
-//    }
-
     fun init(kioskId: String, accessToken: String, utgStrategyRequest: UtgStrategyRequest): List<AgentActionDto> {
         logService.printLog(
             UtgStartLog(
@@ -157,47 +123,6 @@ class UtgService @Autowired constructor(
 
         utgUpdateOrchestrator.editPayment(context, modifiedPayment, randomMenu)
         uiGraphService.changeModified(context.kioskId, modifiedPayment)
-
-        return context.pushedImages
-    }
-
-    fun updateCategoryGraph(
-        kioskId: String, accessToken: String,
-        isInitPayment: Boolean
-    ): List<String> {
-        val context = UtgContext.toBasicContext(kioskId, LogicType.UPDATE)
-
-        //수정된 메뉴, 완료된 메뉴 가져오기
-        val menuList = menuService.getMenus(kioskId, accessToken)
-
-        menuUtgService.updateCategory(context, menuList)
-        if (isInitPayment) {
-            paymentUtgService.initializeGraph(context)
-        }
-
-        return context.pushedImages
-    }
-
-    fun updateMenuGraph(
-        kioskId: String, accessToken: String,
-        isInitPayment: Boolean
-    ): List<String> {
-        val context = UtgContext.toBasicContext(kioskId, LogicType.UPDATE)
-
-        //수정된 메뉴, 완료된 메뉴 가져오기
-        val menuList = menuService.getMenus(kioskId, accessToken)
-
-        menuUtgService.updateMenu(context, menuList)
-        if (isInitPayment) {
-            paymentUtgService.initializeGraph(context)
-        }
-
-        return context.pushedImages
-    }
-
-    fun updatePaymentGraph(kioskId: String): List<String> {
-        val context = UtgContext.toBasicContext(kioskId, LogicType.UPDATE)
-        paymentUtgService.updatePayment(context)
 
         return context.pushedImages
     }
